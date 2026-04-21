dcc5c322682c0bfdc3df97ba46089de9bc83fb5c
Manuel1598 <manuel.weilguni1@gmail.com>
2026-04-21 18:55:30 +0200

    feat: add initial RecBole-native MostPop model skeleton

---
+++ src/models/recbole/custom_models/mostpop_recbole.py
@@ -0,0 +1,54 @@
+import torch
+
+from recbole.model.abstract_recommender import GeneralRecommender
+from recbole.utils import InputType
+
+
+class MostPopRecBole(GeneralRecommender):
+    input_type = InputType.POINTWISE
+
+    def __init__(self, config, dataset):
+        super(MostPopRecBole, self).__init__(config, dataset)
+
+        self.device = config["device"]
+
+        # RecBole field names
+        self.USER_ID = config["USER_ID_FIELD"]
+        self.ITEM_ID = config["ITEM_ID_FIELD"]
+
+        # Number of items in the RecBole dataset
+        self.n_items = dataset.num(self.ITEM_ID)
+
+        # Popularity scores for all item ids in RecBole's internal index space
+        self.item_popularity = torch.zeros(self.n_items, dtype=torch.float32)
+
+        # InterFeat contains the observed interactions
+        item_ids = dataset.inter_feat[self.ITEM_ID]
+
+        # Count item occurrences
+        for item_id in item_ids:
+            self.item_popularity[item_id] += 1.0
+
+        self.item_popularity = self.item_popularity.to(self.device)
+
+    def forward(self, interaction):
+        item = interaction[self.ITEM_ID]
+        return self.item_popularity[item]
+
+    def calculate_loss(self, interaction):
+        # MostPop is non-trainable, but RecBole expects a loss tensor
+        return torch.zeros(1, device=self.device, requires_grad=True)
+
+    def predict(self, interaction):
+        item = interaction[self.ITEM_ID]
+        return self.item_popularity[item]
+
+    def full_sort_predict(self, interaction):
+        user = interaction[self.USER_ID]
+        batch_size = user.shape[0]
+
+        # Return one popularity score vector per user in the batch
+        scores = self.item_popularity.unsqueeze(0).repeat(batch_size, 1)
+
+        # RecBole expects a flattened tensor here
+        return scores.view(-1)
